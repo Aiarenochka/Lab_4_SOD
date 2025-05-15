3624bae268b46df45dfc1854e08df6eef7ac7562
Aiarenochka <kovalcukanastasia18@gmail.com>
2025-05-15 23:18:58 +0300

    Lab_4_SOD

--- src/main/java/main/io/Printer.java
+++ src/main/java/main/io/Printer.java
@@ -8,7 +8,7 @@ public class Printer {
         print("", tree.getRoot(), false);
     }
 
-    private static <T> void print(String prefix, Node<T> node, boolean isLeft) {
+    private static <T extends String> void print(String prefix, Node<T> node, boolean isLeft) {
         if (node != null) {
             System.out.println(prefix + (isLeft ? "├── " : "└── ") + node.key);
             print(prefix + (isLeft ? "│   " : "    "), node.left, true);
